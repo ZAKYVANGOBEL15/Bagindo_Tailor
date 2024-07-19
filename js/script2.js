b08fe54426598bc04291e9c181e96a85eb2a9170
Zaky <145343615+ZAKYVANGOBEL15@users.noreply.github.com>
2024-07-19 13:44:32 +0100

    Add files via upload

--- js/script2.js
+++ js/script2.js
@@ -39,35 +39,15 @@ const swiper = new Swiper('.slide-wrapper', {
 
 
 
-const toggle = document.querySelector('.toggle');
+const toggle = document.querySelector('.humburger');
 
-const sidebar = document.querySelector('.sidebar')
+const sidebar = document.querySelector('.sidebar');
 
-const round = document.querySelector('.round') 
-
-const cont_image = document.querySelector('.cont-image')
-
-const thumb_image = document.querySelector('.thumb-image')
-
-
-toggle.addEventListener('click', () => {
-    sidebar.classList.toggle('sidebar2');
-
-
-    round.addEventListener('click', () => {
-        round.classList.toggle('round_after')
-        
-    })
-
-})
-
-cont_image.addEventListener('click', (e) => {
+toggle.addEventListener('click', ()=> {
     
-    if(e.target.className == 'img-baju') {
-        thumb_image.src = e.target.src
+    toggle.classList.toggle('change')
 
-        
-    } 
+    sidebar.classList.toggle('sidebar_after')
 
 })
 
